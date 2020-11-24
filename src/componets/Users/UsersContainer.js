b41ad295a8b4d819db8ea6c45cee92b2a42a092e
Lyudmila <melina.antipova@gmail.com>
2020-11-24 12:34:50 +0200

    Fix UsersContainer.js

--- src/componets/Users/UsersContainer.js
+++ src/componets/Users/UsersContainer.js
@@ -1,12 +1,43 @@
 import {connect} from "react-redux";
 import Users from "./Users";
+import React from 'react';
 import {
     followerActionCreater,
     setCurrentPageActionCreater, setTotalUsersCountActionCreater,
     setUsersActionCreater,
     unfollowerActionCreater
 } from "../../redux/UsersReduser";
+import * as axios from "axios";
 
+class UsersContainerApi extends React.Component {
+
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items);
+                this.props.setTotalUsersCount(response.data.totalCount);
+            })
+    }
+
+    onCurrentPage = (numberPage) => {
+        this.props.setCurrentPage(numberPage)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${numberPage}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items)
+            })
+    }
+
+    render() {
+        return <Users totalUsersCount={this.props.totalUsersCount}
+                      pageSize={this.props.pageSize}
+                      currentPage={this.props.currentPage}
+                      onCurrentPage={this.onCurrentPage}
+                      users={this.props.users}
+                      follow={this.props.follow}
+                      unfollow={this.props.unfollow}
+        />
+    }
+}
 
 let mapStateToProps = (state) => {
     return {
@@ -37,6 +68,6 @@ let mapDispatchToProps = (dispatch) => {
 }
 
 
-    const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users)
+const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(UsersContainerApi)
 
-    export default UsersContainer;
+export default UsersContainer;
